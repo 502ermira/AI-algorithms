3e3f6cc48841589665af7f34acff6927f41e7049
502ermira <ermiraaa20@gmail.com>
2024-06-17 01:09:16 +0200

    Full project upload: A-Star, GA, DFS, ACO, Q-Learning, SARSA

---
+++ src/QLearning.py
@@ -0,0 +1,37 @@
+import numpy as np
+from collections import defaultdict
+
+
+class QLearning:
+    # To Do: Fill in from here !
+    def __init__(self, actions):
+        self.actions = actions
+        self.alpha = 0.1  
+        self.gamma = 0.85
+        self.actions = actions
+        self.epsilon = 0.01
+        self.q_table = defaultdict(lambda: [0.0, 0.0, 0.0, 0.0])
+
+    def get_action(self, state):
+        # next_action = np.random.choice(self.actions)
+        if np.random.uniform() > self.epsilon:
+            action_values = self.q_table[state]
+            argmax_actions = [] 
+            for i in range(len(action_values)):
+                if action_values[i] == np.max(action_values):
+                    argmax_actions.append(i)
+            next_action = np.random.choice(argmax_actions)
+        else:
+            next_action = np.random.choice(self.actions)
+        if self.epsilon > 0:
+            self.epsilon -= 0.00001  
+        if self.epsilon < 0:
+            self.epsilon = 0
+
+        return next_action
+
+    def learn(self, current_state, current_action, reward, next_state):
+        next_action = np.argmax(self.q_table[next_state])
+        new_q = reward + self.gamma * self.q_table[next_state][int(next_action)]
+        self.q_table[current_state][current_action] = (1 - self.alpha) * self.q_table[current_state][
+            current_action] + self.alpha * new_q
